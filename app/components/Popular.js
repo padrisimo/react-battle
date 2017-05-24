bc3fb05064b9d46e7a274a42283b554830f8f920
padrisimo <nopuedoparardemolar@outlook.com>
2017-05-24 18:08:02 +0200

    setstate

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,37 @@
 var React = require('react');
 
 class Popular extends React.Component{
+	constructor (props){
+		super(props);
+		this.state = {
+			selectedLanguage: 'All'
+		};
+
+		this.updateLanguage = this.updateLanguage.bind(this);
+	}
+	updateLanguage(lang){
+		this.setState(function () {
+			return {
+				selectedLanguage: lang
+			}
+		})
+	}
 	render(){
+		var languages = ['All', 'javaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+		console.log('antes', this);
 		return(
-			<div>Popular!</div>
+			<ul className='languages'>
+				{languages.map(function(lang) {
+					console.log('dentro', this);
+					return(
+						<li
+						onClick = {this.updateLanguage}
+						key={lang}>
+							{lang}
+						</li>
+					)
+				},this)}
+			</ul>
 		)
 	}
 }
