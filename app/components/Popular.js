62f47e0bf26ac10d531c72d236c614b69be65b00
padrisimo <nopuedoparardemolar@outlook.com>
2017-06-07 20:48:47 +0200

    ajax req and Lifecycle Events

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage(props){
 	var languages = ['All', 'javaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -28,17 +29,30 @@ class Popular extends React.Component{
 	constructor (props){
 		super(props);
 		this.state = {
-			selectedLanguage: 'All'
+			selectedLanguage: 'All',
+			repos: null
 		};
 
 		this.updateLanguage = this.updateLanguage.bind(this);
 	}
+	componentDidMount (){
+		this.updateLanguage(this.state.selectedLanguage);
+	}
 	updateLanguage(lang){
 		this.setState(function () {
 			return {
-				selectedLanguage: lang
+				selectedLanguage: lang,
+				repos: null
 			}
-		})
+		});
+		api.fetchPopularRepos(lang)
+	      .then(function (repos) {
+			  this.setState(function () {
+				  return{
+					repos: repos
+				  }
+			  })
+		}.bind(this));
 	}
 	render(){
 		return(
@@ -47,6 +61,7 @@ class Popular extends React.Component{
 					selectedLanguage = {this.state.selectedLanguage}
 					onSelect = {this.updateLanguage}
 				/>
+				{JSON.stringify(this.state.repos, null, 2)}
 			</div>
 
 		)
